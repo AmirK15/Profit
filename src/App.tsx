dcc76a8a8e74ebd249fc3f81c6d443fda2f7804e
AmirK15 <amirkozanov@gmail.com>
2024-07-08 19:52:05 +0600

    PROFIT - add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 export const App = () => {
-    return (
-        <div style={{height: '100vh', display: "flex", justifyContent: "center", alignItems: "center" }}>
-          Profit App
-        </div>
-    );
+  const sadf = 'fds';
+
+  return (
+    <div style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>Profit App</div>
+  );
 };
