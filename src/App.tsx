693ae14786520ba14e785476ebd09dc70eaab5a8
AmirK15 <amirkozanov@gmail.com>
2024-07-09 00:10:49 +0600

    PROFIT - add MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
+import { Box, Button, Container, TextField, Typography } from '@mui/material';
 
 export const App = () => {
-  const sadf = 'fds';
+  const [formText, setFormText] = useState('');
+  const [inputValue, setInputValue] = useState('');
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setFormText(inputValue);
+    setInputValue('');
+  };
 
   return (
-    <div style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>Profit App</div>
+    <Container style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <Box style={{ display: 'flex', flexDirection: 'column', rowGap: '15px' }}>
+        {formText && <Typography style={{ textAlign: 'center' }}>{formText}$</Typography>}
+        <Box
+          style={{ display: 'flex', flexDirection: 'column', rowGap: '10px' }}
+          onSubmit={handleSubmit}
+          component='form'>
+          <TextField
+            type='number'
+            value={inputValue}
+            onChange={e => setInputValue(e.target.value)}
+            label='Price'
+            variant='filled'
+          />
+          <Button type='submit' variant='outlined'>
+            Submit
+          </Button>
+        </Box>
+      </Box>
+    </Container>
   );
 };
